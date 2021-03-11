55cb3c3a6bf0ab45070ac24ea7fa464f0af15521
Cole Kubossek <cole.kubossek@gmail.com>
2021-03-11 01:52:52 +0000

    all working except diagonal

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,50 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'A', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'P', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'I', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'L', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'KAPIL');
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'L', 'O', 'C', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'COLE');
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonals", function() {
+    const result = wordSearch([
+      ['L', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'I', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'G', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'H', 'L', 'O', 'C', 'G'],
+      ['W', 'H', 'C', 'S', 'T', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LIGHT');
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,66 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    let transposedLetters = transpose(letters);
+    diagonal(letters)
+    if (search(letters, word) || search(transposedLetters,word)) {
+      return true;
     }
-}
+    return false;
+  //vertical search
+  };
+  const search = (letters, word) => {
+    let result = false;
+    //horizontal search
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+      if (l.includes(word) || l.split("").reverse().join("").includes(word)) {
+        result = true;
+        break;
+      }
+    }
+    return result;
+  };
 
-module.exports = wordSearch
+  const transpose = function(matrix) {
+    let rowsLength = matrix[0].length;
+    let colsLength = matrix.length;
+    let newArr1 = [];
+    for (let i = 0; i < rowsLength; i++) {
+      let temp = [];
+      for (let j = 0; j < colsLength; j++) {
+        temp.push(matrix[j][i]);
+      }
+      newArr1.push(temp);
+    }
+    return newArr1;
+  };
+  
+//   const diagonal = function(matrix) {
+//     let newArr1 = [];
+//     let rowsLength = matrix[0].length;
+//     let colsLength = matrix.length;
+//     for (let i = 0; i < (rowsLength); i++) {
+//       let temp = [];
+//       let x = i;
+//       for (let j = 0; j <= i; j++) {
+//         temp.push(matrix[x--][j]);
+//       }
+//       console.log(temp)
+//       newArr1.push(temp);
+//     }
+//     for (let i = 7; i >= 0; i--) {
+//         let temp = [];
+//         let y = i;
+//         for (let j = i; j<=i; j++) {
+//           let x = j;
+//           temp.push(matrix[x++][j]);
+//         }
+//         console.log(temp)
+//         newArr1.push(temp);
+//       }
+//     return newArr1;
+//   };
+  
+//   diagonal(result)
+
+
+  module.exports = wordSearch;
